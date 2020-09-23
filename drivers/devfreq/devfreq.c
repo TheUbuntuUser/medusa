a29c99b299ecedeabad29e481f6b7d7b09972109
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-09-23 14:54:15 +0000

    devfreq.c: Add New Devfreq Governors to Whitelist

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -822,6 +822,9 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 		strncmp(str_governor, "powersave", DEVFREQ_NAME_LEN) &&
 		strncmp(str_governor, "simple_ondemand", DEVFREQ_NAME_LEN) &&
 		strncmp(str_governor, "userspace", DEVFREQ_NAME_LEN))
+		strncmp(str_governor, "msm-adreno-plus", DEVFREQ_NAME_LEN))
+		strncmp(str_governor, "balanced", DEVFREQ_NAME_LEN))
+		strncmp(str_governor, "guardian", DEVFREQ_NAME_LEN))
 		return -EINVAL;
 
 	mutex_lock(&devfreq_list_lock);
