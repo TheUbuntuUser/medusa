f3496f883cc4f39391ce47de1fb36b2c5ad3579d
andrew2193 <birthday@hints.or.kr>
2022-10-06 23:03:13 +0900

    build script: Add -j(n) flag to speed up compilation.

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export KBUILD_BUILD_USER="PsychicGirl-"
 echo -e "\033[1;31m*****Compileing Your Kernel*****\033[0m"
 mkdir output
 make -C $(pwd) O=output j2y18lte_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output -j16
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
